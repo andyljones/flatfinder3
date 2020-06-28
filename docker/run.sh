2f124ee7eeba84b66ca1b9ac2e995f6a8127bc96
Andrew Jones <andyjones.ed@gmail.com>
2020-06-28 12:24:36 +0100

    Sets up docker

---
+++ docker/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Launching Jupyter"
+mkdir -p output/logs
+nohup jupyter notebook --notebook-dir . --no-browser --port=5000 --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="" >output/logs/jupyter.log 2>&1 &
+
+echo "Entering loop"
+while sleep 1000; do :; done
